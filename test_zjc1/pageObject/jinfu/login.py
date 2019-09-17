9ab69677006b93f2d13c98779606ad89facdb690
xyzNing <466268124@qq.com>
2019-09-17 16:22:04 +0800

    ss

--- test_zjc1/pageObject/jinfu/login.py
+++
@@ -1,5 +0,0 @@
-from selenium.webdriver.common.by import By
-from configer.basePage import BasePage
-class LoginJF(BasePage):
-    loc_pur=(By.XPATH,"//li[@code='2001']")
-    loc_sup=(By.XPATH,"//li[@code='2002']")
